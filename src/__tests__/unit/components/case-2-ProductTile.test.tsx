8400d110a693a6532720f4fdbe8db4e56d53ce08
Divine Orji <dpkreativ@gmail.com>
2023-02-22 23:49:33 +0100

    Testing product images and text

--- src/__tests__/unit/components/case-2-ProductTile.test.tsx
+++ src/__tests__/unit/components/case-2-ProductTile.test.tsx
@@ -4,15 +4,38 @@ import { render } from '@testing-library/react'
 import ProductTile from '../../../components/ProductTile'
 
 describe('The <ProductTile /> component', () => {
-  it('❌ renders a product tile with name, image and price', () => {
+  const defaultProduct = {
+    id: 12,
+    name: 'Test Product',
+    price: 'from $12.99',
+    image: '/image.png',
+    brand: 'Adidas',
+    createdAt: '2020-01-01T00:00:',
+    isActive: true,
+  }
 
+  it('renders a product tile with name, image and price', () => {
+    const { getByText, getByAltText } = render(
+      <ProductTile {...(defaultProduct as any)} />,
+    )
+
+    expect(getByText(defaultProduct.name)).toBeInTheDocument()
+    expect(getByText(defaultProduct.price)).toBeInTheDocument()
+    expect(getByAltText(defaultProduct.name)).toBeInTheDocument()
   })
 
-  it('❌ renders a product tile with name and price only', () => {
+  it('renders a product tile with name and price only', () => {
+    const { queryByTestId } = render(
+      <ProductTile {...({ ...defaultProduct, image: undefined } as any)} />,
+    )
 
+    expect(queryByTestId('ProductTileImage')).toBeNull()
   })
 
-  it('❌ has no accessibility violations', async () => {
-
+  it('has no accessibility violations', async () => {
+    const { getByAltText } = render(
+      <ProductTile {...(defaultProduct as any)} />,
+    )
+    expect(getByAltText(defaultProduct.name)).toBeInTheDocument()
   })
 })
